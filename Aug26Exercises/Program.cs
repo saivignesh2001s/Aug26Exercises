c618a5ef203a396b7aeb2c51e61a739b3e8bc55d
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-26 14:17:27 +0530

    Add project files.

---
+++ Aug26Exercises/Program.cs
@@ -0,0 +1,53 @@
+﻿using Aug26Reusable;
+namespace Aug26Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of students");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Students[] student = new Students[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the {0} id", i + 1);
+                student[i].ID=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the name");
+                student[i].Name = Console.ReadLine();
+                Console.WriteLine("Enter age");
+                student[i].Age = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < student.Length; i++)
+            {
+                student[i].print();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+    struct Students
+    {
+        public int id;
+        public string name;
+        public int age;
+        public int ID
+        {
+            set { id= value; }
+            get { return id; }
+        }
+        public string Name
+        {
+            set { name= value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age= value; }
+            get { return age; }
+        }
+    public void print()
+    {
+        Console.Write("{0}\t {1}\t {2}\n",id,name,age);
+    }
+}
---
+++ Aug26Reusable/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug26Reusable
+{
+    public class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public int ID
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+        public void print()
+        {
+            Console.Write("{0}\t {1}\t {2}\n", id, name, age);
+        }
+
+    }
+}
